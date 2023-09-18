ca42f6529ed2323fcf2d59e1a4715295a92e4113
Mundo, Jr <111589914+mundojrasu@users.noreply.github.com>
2023-09-17 17:46:58 -0700

    Inital upload

---
+++ routes/userRouter.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const router = express.Router();
+const userController = require("../controllers/userController.js");
+
+router
+  .route("/")
+  .get(userController.getAllUsers)
+  .post(userController.createNewUser);
+
+router
+  .route("/:id")
+  .get(userController.getUserByID)
+  .patch(userController.patchUserById)
+  .delete(userController.deleteUserByID);
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,13 @@
+const dotenv = require("dotenv");
+dotenv.config({ path: "./config.env" }); // this line has to come first since we need to tell where is the config file.
+
+const app = require("./app");
+const port = process.env.PORT || 3000;
+
+app.get("/test", (req, res) => {
+  res.send("This is a test route!");
+});
+
+app.listen(port, () => {
+  console.log(`App running on port ${port}...`);
+});
